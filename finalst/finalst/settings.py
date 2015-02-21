7a8d971603e66b26a5308e8f378e71c523481223
Marko <marko@Markos-MacBook-Pro.local>
2015-02-21 18:10:34 +0100

    change

--- finalst/finalst/settings.py
+++ finalst/finalst/settings.py
@@ -98,7 +98,7 @@ TEMPLATE_DIRS = (
 
 if DEBUG:
     MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'static_only')
+    # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'static_only')
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'media')
     STATICFILES_DIRS = (
         os.path.join(os.path.dirname(BASE_DIR), 'static', 'static'),
